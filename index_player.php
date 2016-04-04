5373d2650ad294672818971defb24e0ab04e2d98
Zicart <Zicart@Hjorths-MacBook-Air.local>
2016-04-04 14:59:37 +0200

    Skift til PC

--- index_player.php
+++ index_player.php
@@ -1,13 +1,32 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html lang="da">
+
 <html>
 <head>
 	<title>Croissant?</title>
+	<meta charset="ISO-8859-1">
+    <meta name="keywords" content="player">
+    <!-- <link rel="stylesheet" href="index_player_css.css"> -->
 </head>
 
 <body>
+	
+	<!-- Logo og Profile i toppen af siden -->
+	<div>
+		<table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center">
+			<tbody>
+				<tr>
+					<td align="left">Indsæt billede her, med tekst under.</td>
+					<td align="right">Indsæt billede her, med tekst under.</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	
+	
 	<!-- Menupunkterne i toppen af siden -->
 	<div>
-		<table width="1000" border="1" cellspacing="0" cellpadding="0" align="center">
+		<table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center">
 			<tbody>
 				<tr>
 					<td align="center">Menupunkt</td>
@@ -15,13 +34,30 @@
 					<td align="center">Menupunkt</td>
 					<td align="center">Menupunkt</td>
 				</tr>
-				
-				
-				
-				
 			</tbody>
 		</table>
 	</div>
+	
+	<!-- Hovedelementerne på siden -->
+	<div>
+		<table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center">
+			<tbody>
+				<tr>
+					<td align="center">Menupunkt</td>
+					<td align="center">Menupunkt</td>
+					<td align="center">Menupunkt</td>
+					<td align="center">Menupunkt</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	
+	
+	
+	
+	
+	
+	
 <?php
 
 ?>
