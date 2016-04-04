febd9d58ee9e999f695ba252d282f627e5e82a82
Zicart <nicolaj_hjorth@hotmail.com>
2016-04-04 22:37:43 +0200

    CSS
    Tilføjet CSS dokument

--- index_player.php
+++ index_player.php
@@ -6,14 +6,15 @@
 	<title>Croissant?</title>
 	<meta charset="ISO-8859-1">
     <meta name="keywords" content="player">
-    <!-- <link rel="stylesheet" href="index_player_css.css"> -->
+	<link rel="stylesheet" href="main.css">
 </head>
 
 <body>
 	
 	<!-- Logo og Profile i toppen af siden -->
 	<div>
-		<table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center">
+		<!-- <table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center"> -->
+		<table>
 			<tbody>
 				<tr>
 					<td align="left">Indsæt billede her, med tekst under.</td>
