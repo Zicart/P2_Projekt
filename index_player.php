1aba01ce0d9050c4999674237a694ed8a5b2764b
Zicart <nicolaj_hjorth@hotmail.com>
2016-04-05 19:26:41 +0200

    Færdig index_player outline
    Færdig index_player outline samt CSS opdateringer

--- index_player.php
+++ index_player.php
@@ -10,55 +10,69 @@
 </head>
 
 <body>
-	
 	<!-- Logo og Profile i toppen af siden -->
 	<div>
-		<!-- <table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center"> -->
-		<table>
+		<table class="topbar border">
 			<tbody>
 				<tr>
-					<td align="left">Indsæt billede her, med tekst under.</td>
-					<td align="right">Indsæt billede her, med tekst under.</td>
+					<td valign="top">
+						<div>
+							<!-- Hjem og Profil knapper -->
+							<table class="boxbox">
+								<tbody>
+									<tr>
+										<td colspan="2" id="homebutton">Indsæt billede her, med tekst under.</td>
+										<td colspan="2" id="profilebutton">Indsæt billede her, med tekst under.</td>
+									</tr>
+								</tbody>
+							</table>
+							<!-- Menupunkter -->
+							<table class="topbar border">
+								<tbody>
+									<tr>
+										<td class="menubutton">(Hold Navn)</td>
+										<td class="menubutton">Kalender</td>
+										<td class="menubutton">Statistikker</td>
+										<td class="menubutton">Nyheder</td>
+									</tr>
+								</tbody>
+							</table>
+							<!-- Paragraf for at lave mellemrum mellem menuer og understående -->
+							<p style="font-size:14px"></p>
+							<div>
+								<table class="boxbox textalign">
+									<tbody>
+										<tr>
+											<!-- Venstre side af midten -->
+											<td>Menupunkt</td>
+											<!-- Højre side af midten -->
+											<td>
+												<table class="boxbox border textalign">
+													<tbody>
+														<tr class="border">
+															<td>KappaKappa123?</td>
+														</tr>
+														<tr>
+															<td>KappaKappa123?</td>
+														</tr>
+													</tbody>
+												</table>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
-	
-	
-	<!-- Menupunkterne i toppen af siden -->
-	<div>
-		<table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center">
-			<tbody>
-				<tr>
-					<td align="center">Menupunkt</td>
-					<td align="center">Menupunkt</td>
-					<td align="center">Menupunkt</td>
-					<td align="center">Menupunkt</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-	
-	<!-- Hovedelementerne på siden -->
-	<div>
-		<table width="1000" height="30" border="1" cellspacing="0" cellpadding="0" align="center">
-			<tbody>
-				<tr>
-					<td align="center">Menupunkt</td>
-					<td align="center">Menupunkt</td>
-					<td align="center">Menupunkt</td>
-					<td align="center">Menupunkt</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-	
-	
-	
-	
-	
-	
-	
+			<!-- Temp. debug		
+
+			-->
+
+
 <?php
 
 ?>
