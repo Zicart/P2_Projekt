44bdd268928742517b0923197329bbd6ba852446
Zicart <nicolaj_hjorth@hotmail.com>
2016-04-18 21:45:41 +0200

    FlexBox
    Ændret det meste til flexbox
    Mangler mindre fixes

--- index_player.php
+++ index_player.php
@@ -10,74 +10,35 @@
 </head>
 
 <body>
-	<!-- Logo og Profile i toppen af siden -->
-	<div>
-		<table class="topbar border">
-			<tbody>
-				<tr>
-					<td valign="top">
-						<div>
-							<!-- Hjem og Profil knapper -->
-							<table class="boxbox">
-								<tbody>
-									<tr>
-										<td colspan="2" id="homebutton">
-											<a href="">
-												<img src="\P2_Projekt\images\bedrefodbold_logo.png" width="128" height="128" alt="">
-											</a>
-										</td>
-										<td colspan="2" id="profilebutton">
-											<a>
-												<img src="\P2_Projekt\images\profil.png" width="60" height="80" alt="">
-											</a>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-							<!-- Menupunkter -->
-							<table class="topbar border">
-								<tbody>
-									<tr>
-										<td class="menubutton">(Hold Navn)</td>
-										<td class="menubutton">Kalender</td>
-										<td class="menubutton">Statistikker</td>
-										<td class="menubutton">Nyheder</td>
-									</tr>
-								</tbody>
-							</table>
-							<!-- Paragraf for at lave mellemrum mellem menuer og understående -->
-							<p style="font-size:14px"></p>
-							<div>
-								<table class="boxbox textalign">
-									<tbody>
-										<tr>
-											<!-- Venstre side af midten -->
-											<td>
-												<iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;hl=da&amp;bgcolor=%23ffffff&amp;src=hq7gp5ii3mm04spr9i237j4o5c%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FCopenhagen" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-											</td>
-											<!-- Højre side af midten -->
-											<td>
-												<table class="boxbox border textalign">
-													<tbody>
-														<tr class="border">
-															<td>KappaKappa123?</td>
-														</tr>
-														<tr>
-															<td>KappaKappa123?</td>
-														</tr>
-													</tbody>
-												</table>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+	<div class="flex-homeprofile center">
+		<div class="flex-home">
+			<a href="">
+				<img src="\P2_Projekt\images\bedrefodbold_logo.png" width="96" height="96" alt="">
+			</a>
+		</div>
+		<div class="flex-profile">
+			<a href="">
+				<img src="\P2_Projekt\images\profil.png" width="60" height="80" alt="">
+			</a>
+		</div>
 	</div>
+	
+	<p></p>
+	
+	<div class="flex-menubuttons center">
+		<a class="flex-menuitem btn" href="">Holdnavn</a>
+		<a class="flex-menuitem btn" href="">Kalender</a>
+		<a class="flex-menuitem btn" href="">Statistikker</a>
+		<a class="flex-menuitem btn" href="">Nyheder</a>
+	</div>
+
+	<p></p>
+
+	<div class="flex-menubuttons center">
+
+	</div>
+
+
 			<!-- Temp. debug		
 
 			-->
